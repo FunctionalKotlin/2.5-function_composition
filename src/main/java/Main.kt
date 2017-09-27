b4dbae5063bab3610f98ce16919377cc015b4add
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-27 21:24:05 +0200

    Ugh, imperative programming is sooo ugly

---
+++ src/main/java/Main.kt
@@ -0,0 +1,40 @@
+// Copyright © FunctionalKotlin.com 2017. All rights reserved.
+
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.Parser
+import java.text.NumberFormat
+import java.util.*
+
+fun main(args: Array<String>) {
+    val prices = "[\"10\", \"5\", \"null\", \"20\", \"0\"]"
+
+    val formattedPrices = formatPrices(prices)
+
+    println("The list of prices is: $formattedPrices")
+}
+
+fun formatPrices(json: String): List<String> {
+    val jsonArray = (Parser().parse(StringBuilder(json)) as JsonArray<String>).toList()
+
+    val labels = mutableListOf<String>()
+
+    for (element in jsonArray) {
+        val price = element.toIntOrNull()
+
+        if (price != null) {
+            var label: String = ""
+
+            if (price == 0) {
+                label = "Free"
+            } else {
+                val formatter = NumberFormat.getCurrencyInstance(Locale("es", "ES"))
+
+                label = formatter.format(price)
+            }
+
+            labels.add(label)
+        }
+    }
+
+    return labels
+}
